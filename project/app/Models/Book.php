f1e2e9e02da49705c73a5a1952569822e8cc0760
Lets <lets@pop-os.localdomain>
2021-03-29 18:07:32 -0300

    relacionamento

--- project/app/Models/Book.php
+++ project/app/Models/Book.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     protected $fillable = ['title', 'author', 'description'];
+
+    public function employer()
+    {
+        return $this->belongsTo(User::class,'employer_id','id');
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class,'user_id','id');
+    }
 }
